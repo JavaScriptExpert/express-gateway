650ea4bd1048e71c37bbaf666f3147cfadf8ddc5
DrMegavolt <sergeykuc@gmail.com>
2017-08-11 19:21:38 +0300

    helpers

--- e2e/common/gateway.helper.js
+++ e2e/common/gateway.helper.js
@@ -1,11 +1,47 @@
-module.exports.setGatewayConfig = function ({dir, newConfig}) {
+const net = require('net');
+const yaml = require('js-yaml');
+const fs = require('fs');
+module.exports.setGatewayConfig = function ({gatewayConfigPath, newConfig}) {
+  fs.writeFileSync(gatewayConfigPath, yaml.dump(newConfig));
+};
+module.exports.getGatewayConfig = function ({gatewayConfigPath}) {
+  let content = fs.readFileSync();
+  return yaml.load(content);
+};
 
+module.exports.findOpenPortNumbers = function (count = 1) {
+  let completeCount = 0;
+  const ports = [];
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < count; i++) {
+      const server = net.createServer();
+
+      server.listen(0);
+
+      server.on('listening', () => {
+        ports.push(server.address().port);
+
+        server.once('close', () => {
+          completeCount++;
+
+          if (completeCount === count) {
+            resolve(ports);
+          }
+        });
+        server.close();
+      });
+
+      server.on('error', (err) => {
+        reject(err);
+      });
+    }
+  });
 };
 
 module.exports.startGatewayInstance = function ({dir}) {
   return Promise.resolve().then(files => {
     testGatewayConfigPath = path.join(tempPath, 'gateway.config.yml');
-    return util.promisify(findOpenPortNumbers)(4);
+    return this.findOpenPortNumbers(4);
   })
       .then(ports => {
         gatewayPort = ports[0];
